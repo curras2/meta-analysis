2701938acbe1d3c3c8ea544044c83e69792fdf07
Matheus Correa <matheus.barrosc@gmail.com>
2025-02-10 18:05:17 -0300

    Organização geral do código.

--- analysis.py
+++ analysis.py
@@ -19,3 +19,122 @@ def banrate_analysis(team_league_df):
     banrate = (ban_counts / total_games) *100
     
     return banrate
+
+def champ_winrate_analysis(player_league_df):
+    champs_df = player_league_df[["champion", "result"]]
+    wr_champs_df = champs_df.groupby("champion").agg(
+    winrate=("result", "mean"),
+    count=("result", "count")).reset_index()
+    
+    wr_champs_df = wr_champs_df.rename(columns={"result": "winrate"})
+    wr_champs_df["winrate"] = wr_champs_df["winrate"] * 100
+
+    return wr_champs_df
+
+def side_winrate_analysis(team_league_df):
+    side_df = team_league_df[["side", "result"]]
+    wr_side_df = side_df.groupby("side").agg(
+    winrate=("result", "mean"),
+    count=("result", "count")).reset_index()
+    
+    wr_side_df = wr_side_df.rename(columns={"result": "winrate"})
+    wr_side_df["winrate"] = wr_side_df["winrate"] * 100
+
+    return wr_side_df
+
+def elder_winrate_analysis(team_league_df):
+    elder_df = team_league_df[["elders", "result"]]
+    elder_df = elder_df[elder_df["elders"] != 0]
+    wr_elder_df = elder_df.agg(
+        winrate=("result", "mean"),
+        count=("result", "count")
+    )
+    
+    return wr_elder_df
+
+def baron_winrate_analysis(team_league_df):
+    baron_df = team_league_df[["barons", "result"]]
+    baron_df = baron_df[baron_df["barons"] != 0]
+    wr_baron_df = baron_df.agg(
+    winrate=("result", "mean"),
+    count=("result", "count"))
+
+    return wr_baron_df
+
+def herald_winrate_analysis(team_league_df):
+    herald_df = team_league_df[["heralds", "result"]]
+    herald_df = herald_df[herald_df["heralds"] != 0]
+    wr_herald_df = herald_df.agg(
+    winrate=("result", "mean"),
+    count=("result", "count"))
+
+    return wr_herald_df
+
+def void_grub_winrate_analysis(team_league_df):
+    void_grub_df = team_league_df[["void_grubs", "result"]]
+    void_grub_df = void_grub_df[void_grub_df["void_grubs"] > 3]
+    wr_void_grub_df = void_grub_df.agg(
+    winrate=("result", "mean"),
+    count=("result", "count"))
+
+    return wr_void_grub_df
+
+def soul_winrate_analysis(team_league_df):
+    soul = team_league_df[["elementaldrakes", "result"]]
+    soul = soul[soul["elementaldrakes"] == 4]
+    wr_soul = soul.agg(
+    winrate=("result", "mean"),
+    count=("result", "count"))
+
+    return wr_soul
+
+def dragon_soul_winrate_analysis(team_league_df):
+    dragon_soul_df = team_league_df[["elementaldrakes", "infernals", "mountains", "clouds", "oceans", "chemtechs", "hextechs", "result"]]
+    dragon_soul_df = dragon_soul_df[dragon_soul_df["elementaldrakes"] == 4]
+
+    def get_soul(row):
+        if row["infernals"] >= 2:
+            return "Infernal"
+        elif row["mountains"] >= 2:
+            return "Mountain"
+        elif row["clouds"] >= 2:
+            return "Cloud"
+        elif row["oceans"] >= 2:
+            return "Ocean"
+        elif row["chemtechs"] >= 2:
+            return "Chemtech"
+        elif row["hextechs"] >= 2:
+            return "Hextech"
+        else:
+            return "Unknown"
+
+    dragon_soul_df["soul"] = dragon_soul_df.apply(get_soul, axis=1)
+    
+    wr_dragon_soul_df = dragon_soul_df.groupby("soul").agg(
+    winrate=("result", "mean"),
+    count=("result", "count")).reset_index()
+    
+    wr_dragon_soul_df = wr_dragon_soul_df.rename(columns={"result":"winrate"})
+    
+    wr_dragon_soul_df["winrate"] = wr_dragon_soul_df["winrate"] * 100
+
+    return wr_dragon_soul_df
+
+def game_length_analysis(team_league_df):
+    game_length_df = team_league_df[["gameid", "gamelength"]]
+    game_length_df = game_length_df.groupby("gameid")["gamelength"].mean().reset_index()
+
+    game_length_df = game_length_df["gamelength"].mean()
+
+    def calculate_time_seconds(sec_time):
+        sec_time = int(sec_time)
+        minutes = sec_time // 60
+        seconds = sec_time % 60
+
+        time_return = f"{minutes:02}:{seconds:02}"
+
+        return time_return
+    
+    game_length_mean = calculate_time_seconds(game_length_df)
+
+    return game_length_mean
--- main.py
+++ main.py
@@ -11,118 +11,25 @@ leagues_list = [
     ]
 
 for i in leagues_list:
+    # Dataframes principais
     league_df = df_gen.create_league_df(lol_df, i)
 
     league_columns_filtered_df = df_gen.define_columns_df(league_df)
 
     team_league_df = df_gen.create_team_league_df(league_columns_filtered_df)
 
-    pickrate = an.pickrate_analysis(team_league_df)
-    banrate = an.banrate_analysis(team_league_df)
-
     player_league_df = df_gen.create_player_league_df(league_columns_filtered_df)
 
-    champs_df = player_league_df[["champion", "result"]]
-    wr_champs_df = champs_df.groupby("champion").agg(
-    winrate=("result", "mean"),
-    count=("result", "count")).reset_index()
-    
-    wr_champs_df = wr_champs_df.rename(columns={"result": "winrate"})
-    wr_champs_df["winrate"] = wr_champs_df["winrate"] * 100
-
-    # print(wr_champs_df.sort_values(by="winrate", ascending=False))
-
-    side_df = team_league_df[["side", "result"]]
-    wr_side_df = side_df.groupby("side").agg(
-    winrate=("result", "mean"),
-    count=("result", "count")).reset_index()
-    
-    wr_side_df = wr_side_df.rename(columns={"result": "winrate"})
-    wr_champs_df["winrate"] = wr_champs_df["winrate"] * 100
-
-    # print(wr_side_df.sort_values(by="winrate", ascending=False))
-
-    elder_df = team_league_df[["elders", "result"]]
-    elder_df = elder_df[elder_df["elders"] != 0]
-    wr_elder_df = elder_df.agg(
-    winrate=("result", "mean"),
-    count=("result", "count"))
-
-    # print(wr_elder_df)
-
-    baron_df = team_league_df[["barons", "result"]]
-    baron_df = baron_df[baron_df["barons"] != 0]
-    wr_baron_df = baron_df.agg(
-    winrate=("result", "mean"),
-    count=("result", "count"))
-
-    # print(wr_baron_df)
-
-    herald_df = team_league_df[["heralds", "result"]]
-    herald_df = herald_df[herald_df["heralds"] != 0]
-    wr_herald_df = herald_df.agg(
-    winrate=("result", "mean"),
-    count=("result", "count"))
-
-    # print(wr_herald_df)
-
-    void_grub_df = team_league_df[["void_grubs", "result"]]
-    void_grub_df = void_grub_df[void_grub_df["void_grubs"] > 3]
-    wr_void_grub_df = void_grub_df.agg(
-    winrate=("result", "mean"),
-    count=("result", "count"))
-
-    # print(wr_void_grub_df)
-
-    elementaldrake_df = team_league_df[["elementaldrakes", "result"]]
-    elementaldrake_df = elementaldrake_df[elementaldrake_df["elementaldrakes"] == 4]
-    wr_elementaldrake_df = elementaldrake_df.agg(
-    winrate=("result", "mean"),
-    count=("result", "count"))
-
-    # print(wr_elementaldrake_df)
-
-    drakes_df = team_league_df[["elementaldrakes", "infernals", "mountains", "clouds", "oceans", "chemtechs", "hextechs", "result"]]
-    drakes_df = drakes_df[drakes_df["elementaldrakes"] == 4]
-
-    def get_soul(row):
-        if row["infernals"] >= 2:
-            return "Infernal"
-        elif row["mountains"] >= 2:
-            return "Mountain"
-        elif row["clouds"] >= 2:
-            return "Cloud"
-        elif row["oceans"] >= 2:
-            return "Ocean"
-        elif row["chemtechs"] >= 2:
-            return "Chemtech"
-        elif row["hextechs"] >= 2:
-            return "Hextech"
-        else:
-            return "Unknown"
-
-    drakes_df["soul"] = drakes_df.apply(get_soul, axis=1)
-    wr_drakes_df = drakes_df.groupby("soul").agg(
-    winrate=("result", "mean"),
-    count=("result", "count")).reset_index()
-    wr_drakes_df = wr_drakes_df.rename(columns={"result":"winrate"})
-    wr_drakes_df["winrate"] = wr_drakes_df["winrate"] * 100
-
-    # print(wr_drakes_df.sort_values(by="winrate", ascending=False))
-
-    game_length_df = team_league_df[["gameid", "gamelength"]]
-    game_length_df = game_length_df.groupby("gameid")["gamelength"].mean().reset_index()
-
-    game_length_df = game_length_df["gamelength"].mean()
-
-    def calculate_time_seconds(sec_time):
-        sec_time = int(sec_time)
-        minutes = sec_time // 60
-        seconds = sec_time % 60
-
-        time_return = f"{minutes:02}:{seconds:02}"
-
-        return time_return
-    
-    print(calculate_time_seconds(game_length_df))
+    # Análise
+    pickrate = an.pickrate_analysis(team_league_df)
+    banrate = an.banrate_analysis(team_league_df)
+    wr_champs_df = an.champ_winrate_analysis(player_league_df)
+    wr_side_df = an.side_winrate_analysis(team_league_df)
+    wr_elder_df = an.elder_winrate_analysis(team_league_df)
+    wr_baron_df = an.baron_winrate_analysis(team_league_df)
+    wr_herald_df = an.herald_winrate_analysis(team_league_df)
+    wr_void_grub_df = an.void_grub_winrate_analysis(team_league_df)
+    wr_soul_df = an.soul_winrate_analysis(team_league_df)
+    wr_dragon_soul_df = an.dragon_soul_winrate_analysis(team_league_df)
+    game_length_mean = an.game_length_analysis(team_league_df)
     

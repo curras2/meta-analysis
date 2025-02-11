858a6efdbd28429b6b520e772fa1dce718d2e486
Matheus Correa <matheus.barrosc@gmail.com>
2025-02-11 09:33:30 -0300

    Criação de funções para upsert em todas as coleções do banco de dados.

--- db_conn.py
+++ db_conn.py
@@ -12,3 +12,34 @@ objectives_winrate_collection = conn[ext.objectives_winrate_collection]
 side_winrate_collection = conn[ext.side_winrate_collection]
 dragon_soul_winrate_collection = conn[ext.dragon_soul_winrate_collection]
 game_length_mean_collection = conn[ext.game_length_mean_collection]
+
+def upsert_record(collection, query, new_data):
+    collection.update_one(query, {"$set": new_data}, upsert=True)
+
+def upsert_banrate_record(data):
+    query = {"_id": 1}
+    upsert_record(banrate_collection, query, data)
+
+def upsert_champion_winrate_record(data):
+    query = {"_id": 1}
+    upsert_record(champion_winrate_collection, query, data)
+
+def upsert_pickrate_record(data):
+    query = {"_id": 1}
+    upsert_record(pickrate_collection, query, data)
+
+def upsert_objectives_winrate_record(data):
+    query = {"_id": 1}
+    upsert_record(objectives_winrate_collection, query, data)
+
+def upsert_side_winrate_record(data):
+    query = {"_id": 1}
+    upsert_record(side_winrate_collection, query, data)
+
+def upsert_dragon_soul_winrate_record(data):
+    query = {"_id": 1}
+    upsert_record(dragon_soul_winrate_collection, query, data)
+
+def upsert_game_length_mean_record(data):
+    query = {"_id": 1}
+    upsert_record(game_length_mean_collection, query, data)
